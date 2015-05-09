52378f5d6859df09217fb60ccf56034b0652a249
joelramsey <joel.d.ramsey@gmail.com>
2015-05-09 01:28:45 +0000

    Mailgun setup continued

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   address:           'smtp.mailgun.org',
   user_name:         ENV['MAILGUN_SMTP_LOGIN'],
   password:          ENV['MAILGUN_SMTP_PASSWORD'],
-  domain:            'https://api.mailgun.net/v2/app791986fb9f3c4e50aa4978d395fc2ba9.mailgun.org',
+  domain:            'https://api.mailgun.net/v3/appc16bc432e87a4132aa8048b5fc13309c.mailgun.org',
   authentication:    :plain,
   content_type:      'text/html',
 }
